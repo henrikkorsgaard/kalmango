d1aabd2ff45118026d8efe24b9dcaec2db3dd6d9
Henrik Korsgaard <korsgaard@cs.au.dk>
2018-05-14 18:33:09 +0200

    well, I did *something*

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# go-kalman
+# A poor man's Kalman filter in Golang
+
+This is my attempt on translating [kalmanjs](https://github.com/wouterbulten/kalmanjs) to Golang.
+
+All credit to @wouterbulten for the orignal library and thinking.
+
+## Disclaimer
+I have zero knowledge of or experience with Kalman filters. This seem to work the same way as Kalmanjs, but I have no clue. Yes, this is reality when UI and web developers get caught up in complex development.
--- main.go
+++ main.go
@@ -1,9 +1,48 @@
-package main
+package Kalmango
 
-import (
-  "fmt"
-)
+type JSONfile struct {
+	NoiseConstant         []int     `json:"noise_constant"`
+	NoiseConstantKalman   []float64 `json:"noise_constant_kalman"`
+	NoiseConstantGoKalman []float64 `json:"noise_constant_gokalman"`
+}
 
-func main(){
-    fmt.Println("hej");
+type Kalman struct {
+	R float64
+	Q float64
+	A float64
+	B float64
+	C float64
+
+	cov float64
+	x   float64
+}
+
+func NewKalmanFilter(r float64, q float64, a float64, b float64, c float64) (kal Kalman) {
+	kal = Kalman{R: r, Q: q, A: a, B: b, C: c}
+	return kal
+}
+
+func (k *Kalman) Filter(z float64, u float64) float64 {
+	if k.cov == 0 {
+		k.x = (1 / k.C) * z
+		k.cov = (1 / k.C) * k.Q * (1 / k.C)
+	} else {
+		predX := k.predict(u)
+		predCov := k.uncertainty()
+
+		K := predCov * k.C * (1 / ((k.C * predCov * k.C) + k.Q))
+
+		k.x = predX + K*(z-(k.C*predX))
+		k.cov = predCov - (K * k.C * predCov)
+	}
+
+	return k.x
+}
+
+func (k *Kalman) predict(u float64) float64 {
+	return (k.A * k.x) + (k.B * u)
+}
+
+func (k *Kalman) uncertainty() float64 {
+	return ((k.A * k.cov) * k.A) + k.R
 }
